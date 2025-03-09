89bc508322eafbfe42a3bda303a057b129a388b3
Matt McCartney <mattrmccartney@gmail.com>
2025-03-09 14:33:35 -0400

    Filled in the initdb.js file, creating the sqlite table.

--- Project1/initdb.js
+++ Project1/initdb.js
@@ -1,1 +1,27 @@
 // file to initialize the sqlite database
+const sqlite3 = require('sqlite3').verbose();
+let db = new sqlite3.Database('./characters.db', (err) => {
+    if (err) {
+        console.error(err.message);
+    }
+    console.log('Connected to the SQlite database.');
+});
+
+// create table
+db.serialize(() => {
+  db.run(`CREATE TABLE IF NOT EXISTS characters (
+	CharacterId INTEGER PRIMARY KEY AUTOINCREMENT,
+    CharacterName TEXT,
+    Token BLOB,
+    Class TEXT,
+    Strength INTEGER,
+    Dexterity INTEGER,
+    Constitution INTEGER,
+    Wisdom INTEGER,
+    Intelligence INTEGER,
+    Charisma INTEGER,
+    Level INTEGER
+  )`);
+});
+
+module.exports = db;
