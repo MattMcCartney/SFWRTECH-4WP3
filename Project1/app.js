12c044025355d0e14b404e892a8f05eb2410a7b3
Matt McCartney <mattrmccartney@gmail.com>
2025-03-09 15:34:26 -0400

    Added content to the application file.

--- Project1/app.js
+++ Project1/app.js
@@ -1,1 +1,26 @@
 // file for the application code
+const express = require('express');
+const mustacheExpress = require('mustache-express');
+const bodyParser = require('body-parser');
+const multer = require('multer');
+const path = require('path');
+
+const app = express();
+
+// Mustache Templating Engine Setup
+app.engine('mustache', mustacheExpress());
+app.set('view engine', 'mustache');
+app.set('views', __dirname + '/views');
+
+// Middleware
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: false }));
+const upload = multer({ dest: 'uploads/' });
+
+// Routes
+app.use(require('./index'));
+
+// Start server
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+});
